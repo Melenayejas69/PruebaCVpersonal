5671b4afd9a950803944916b8ff275f43e80c829
María Elena Yejas <78365590+Melenayejas69@users.noreply.github.com>
2021-03-09 20:01:38 -0400

    Funciones Javascript//2do corte

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarSkills(){
+    document.getElementsByClassName('containerSkills').style.display='block';
+}
+var fecha = new date();
+var ano = fecha.getFullYear();
+alert('El año actual es: '+ano);
